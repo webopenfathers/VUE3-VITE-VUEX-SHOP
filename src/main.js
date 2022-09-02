53292797e882ef705402e413cb9567b2f38cd7d4
webopenfathers <1290443925@qq.com>
2022-09-02 21:22:24 +0800

    2022 9-2 21:21 路由配置和404页面捕获

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import { createApp } from 'vue'
+// import './style.css'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import App from './App.vue'
+
+import router from './router'
+
+
+const app = createApp(App)
+
+app.use(router)
+
+app.use(ElementPlus)
+
+import 'virtual:windi.css'
+
+app.mount('#app')
