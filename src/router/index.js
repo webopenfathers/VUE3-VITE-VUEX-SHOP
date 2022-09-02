e485b381c97229916e1be51ba62e1864a0d3f76e
webopenfathers <1290443925@qq.com>
2022-09-02 22:01:50 +0800

    2022 9-2 22:01 登录页开发

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
 import Index from '@/pages/index.vue'
-import About from '@/pages/about.vue'
+import Login from '@/pages/login.vue'
 import NotFound from '@/pages/404.vue'
 
 const routes = [
@@ -10,8 +10,8 @@ const routes = [
         component: Index
     },
     {
-        path: '/about',
-        component: About
+        path: '/login',
+        component: Login
     },
     {
         path: '/:pathMatch(.*)*',
