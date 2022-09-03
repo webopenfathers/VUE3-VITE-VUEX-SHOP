b031cb745acdcb1c9cadd11984f7560ba6baf8bc
webopenfathers <1290443925@qq.com>
2022-09-03 13:47:43 +0800

    2022 9-3 13:47 公共头部开发-刷新和全屏

--- src/layout/components/FHeader.vue
+++ src/layout/components/FHeader.vue
@@ -4,10 +4,18 @@
       ><el-icon class="mr-1"><ElemeFilled /></el-icon> coderZbw
     </span>
     <el-icon class="icon-btn"><Fold /></el-icon>
-    <el-icon class="icon-btn"><Refresh /></el-icon>
+    <el-tooltip effect="dark" content="刷新" placement="bottom">
+      <el-icon class="icon-btn" @click="handleRefresh"><Refresh /></el-icon>
+    </el-tooltip>
     <div class="ml-auto flex items-center">
-      <el-icon class="icon-btn"><FullScreen /></el-icon>
-      <el-dropdown class="dropdown">
+      <el-tooltip effect="dark" content="全屏" placement="bottom">
+        <el-icon class="icon-btn" @click="toggle">
+          <FullScreen v-if="!isFullscreen" />
+          <Aim v-else />
+        </el-icon>
+      </el-tooltip>
+
+      <el-dropdown class="dropdown" @command="handleCommand">
         <span class="flex items-center text-light-50">
           <el-avatar class="mr-2" :size="25" :src="$store.state.user.avatar" />
           {{ $store.state.user.username }}
@@ -17,15 +25,56 @@
         </span>
         <template #dropdown>
           <el-dropdown-menu>
-            <el-dropdown-item>修改密码</el-dropdown-item>
-            <el-dropdown-item>退出登录</el-dropdown-item>
+            <el-dropdown-item command="rePassword">修改密码</el-dropdown-item>
+            <el-dropdown-item command="logout">退出登录</el-dropdown-item>
           </el-dropdown-menu>
         </template>
       </el-dropdown>
     </div>
   </div>
 </template>
-<script>
+<script setup>
+import { showModal, toast } from "@/utils/util.js";
+import { logout } from "@/api/manager.js";
+import { useRouter } from "vue-router";
+import { useStore } from "vuex";
+import { useFullscreen } from "@vueuse/core";
+
+const {
+  // 是否全屏状态
+  isFullscreen,
+  // 切换全屏
+  toggle,
+} = useFullscreen();
+const router = useRouter();
+const store = useStore();
+
+const handleCommand = (c) => {
+  switch (c) {
+    case "logout":
+      handleLogout();
+      break;
+    case "rePassword":
+      console.log("修改密码");
+      break;
+  }
+};
+
+// 刷新
+const handleRefresh = () => location.reload();
+
+function handleLogout() {
+  showModal("是否要退出登录?").then((res) => {
+    console.log("退出登录");
+    logout().finally(() => {
+      store.dispatch("logout");
+      // 跳转回登录页
+      router.push("/login");
+      // 提示退出登陆成功
+      toast("退出登陆成功");
+    });
+  });
+}
 </script>
 <style scoped>
 .f-header {
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,25 +1,6 @@
 <template>
   <div>后台首页{{ $store.state.user.username }}</div>
-  <el-button @click="handleLogout">退出</el-button>
 </template>
 
 <script setup>
-import { showModal, toast } from "@/utils/util.js";
-import { logout } from "@/api/manager.js";
-import { useRouter } from "vue-router";
-import { useStore } from "vuex";
-const router = useRouter();
-const store = useStore();
-function handleLogout() {
-  showModal("是否要退出登录?").then((res) => {
-    console.log("退出登录");
-    logout().finally(() => {
-      store.dispatch("logout");
-      // 跳转回登录页
-      router.push("/login");
-      // 提示退出登陆成功
-      toast("退出登陆成功");
-    });
-  });
-}
 </script>
