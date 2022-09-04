77de7f3e56923cc6ecfe4293379ff68c29635230
webopenfathers <1290443925@qq.com>
2022-09-04 17:11:42 +0800

    2022 9-4 17:11 全局过渡动画

--- src/layout/admin.vue
+++ src/layout/admin.vue
@@ -15,10 +15,15 @@
         <f-tag-list></f-tag-list>
         <!-- 主内容 -->
         <router-view v-slot="{ Component }">
-          <!-- keep-alive页面缓存 -->
-          <keep-alive :max="10">
-            <component :is="Component"></component>
-          </keep-alive>
+          <!-- 全局过渡动画 -->
+          <transition name="fade">
+            <!-- keep-alive页面缓存 -->
+            <keep-alive :max="10">
+              <component :is="Component"></component>
+            </keep-alive>
+            <!-- keep-alive页面缓存 -->
+          </transition>
+          <!-- 全局过渡动画 -->
         </router-view>
       </el-main>
     </el-container>
@@ -32,5 +37,27 @@ import FTagList from "./components/FTagList.vue";
 <style scoped>
 .el-aside {
   transition: all 0.2s;
+}
+
+.fade-enter-from {
+  opacity: 0;
+}
+.fade-enter-to {
+  opacity: 1;
+}
+.fade-leave-from {
+  opacity: 1;
+}
+.fade-leave-to {
+  opacity: 0;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.3s;
+}
+
+.fade-enter-active {
+  transition-delay: 0.3s;
 }
 </style>
