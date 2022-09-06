42679735063bba5299d3c9917ebb2a434940a162
webopenfathers <1290443925@qq.com>
2022-09-07 00:04:46 +0800

    2022 9-6 0:04 图库列表组件开发

--- src/components/ImageMain.vue
+++ src/components/ImageMain.vue
@@ -1,8 +1,30 @@
 <template>
   <el-main class="image-main" v-loading="loading">
     <!-- 图片编辑 -->
-    <div class="top">
-      <div v-for="(item, index) in list" :key="index">{{ item.url }}</div>
+    <div class="top p-3">
+      <el-row :gutter="10">
+        <el-col
+          :span="6"
+          :offset="0"
+          v-for="(item, index) in list"
+          :key="index"
+        >
+          <el-card
+            shadow="hover"
+            class="relative mb-3"
+            :body-style="{ padding: 0 }"
+          >
+            <el-image :src="item.url" fit="cover" class="w-full h-[150px]">
+            </el-image>
+            <!-- 标题 -->
+            <div class="image-title">{{ item.name }}</div>
+            <div class="flex items-center justify-center p-2">
+              <el-button type="primary" size="small" text>重命名</el-button>
+              <el-button type="primary" size="small" text>删除</el-button>
+            </div>
+          </el-card>
+        </el-col>
+      </el-row>
     </div>
     <!-- 分页区域 -->
     <div class="bottom">
@@ -74,5 +96,13 @@ defineExpose({
   right: 0;
   left: 0;
   @apply flex items-center justify-center;
+}
+
+.image-title {
+  position: absolute;
+  top: 122px;
+  left: -1px;
+  right: -1px;
+  @apply text-sm truncate text-gray-100 bg-opacity-50 bg-gray-800 px-2 py-1;
 }
 </style>
