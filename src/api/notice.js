f9247a4a72cc7958e543182f655890ab33567472
webopenfathers <1290443925@qq.com>
2022-09-07 21:41:12 +0800

    2022 9-7 21:40 公告新增功能实现

--- src/api/notice.js
+++ src/api/notice.js
@@ -3,3 +3,7 @@ import axios from '@/axios'
 export function getNoticeList(page) {
     return axios.get(`/admin/notice/${page}`)
 }
+
+export function createNotice(data) {
+    return axios.post(`/admin/notice`, data)
+}
--- src/pages/notice/list.vue
+++ src/pages/notice/list.vue
@@ -2,7 +2,9 @@
   <el-card shadow="never" class="border-0">
     <!-- 新增|刷新 -->
     <div class="flex items-center justify-between mb-4">
-      <el-button type="primary" size="small">新增</el-button>
+      <el-button type="primary" size="small" @click="handleCreate"
+        >新增</el-button
+      >
       <el-tooltip effect="dark" content="刷新数据" placement="top">
         <el-button text @click="getData">
           <el-icon :size="20"><Refresh /></el-icon>
@@ -40,11 +42,38 @@
         @current-change="getData"
       />
     </div>
+    <!-- 弹框 -->
+    <FormDrawer ref="formDrawerRef" title="新增" @submit="handleSubmit">
+      <el-form
+        :model="form"
+        ref="formRef"
+        :rules="rules"
+        label-width="80px"
+        :inline="false"
+      >
+        <el-form-item label="公告标题" prop="title">
+          <el-input
+            v-model="form.title"
+            placeholder="请输入公告标题"
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="公告内容" prop="content">
+          <el-input
+            v-model="form.content"
+            placeholder="请输入公告内容"
+            type="textarea"
+            :rows="5"
+          ></el-input>
+        </el-form-item>
+      </el-form>
+    </FormDrawer>
   </el-card>
 </template>
 <script setup>
-import { ref } from "@vue/reactivity";
-import { getNoticeList } from "@/api/notice";
+import { reactive, ref } from "@vue/reactivity";
+import { getNoticeList, createNotice } from "@/api/notice";
+import FormDrawer from "@/components/FormDrawer.vue";
+import { toast } from "../../utils/util";
 
 const tableData = ref([]);
 // 加载动画
@@ -76,5 +105,39 @@ getData();
 // 删除
 const handleDelete = (id) => {
   console.log(id);
+};
+
+//表单部分
+const formDrawerRef = ref(null);
+const formRef = ref(null);
+const form = reactive({
+  title: "",
+  content: "",
+});
+
+const rules = {
+  title: [{ required: true, message: "公告名称不能为空", trigger: "blur" }],
+  content: [{ required: true, message: "公告内容不能为空", trigger: "blur" }],
+};
+const handleSubmit = () => {
+  formRef.value.validate((valid) => {
+    if (!valid) return;
+
+    formDrawerRef.value.showLoading();
+    createNotice(form)
+      .then((res) => {
+        toast("新增成功");
+        getData(1);
+        formDrawerRef.value.close();
+      })
+      .finally(() => {
+        formDrawerRef.value.hideLoading();
+      });
+  });
+};
+
+// 打开弹框
+const handleCreate = () => {
+  formDrawerRef.value.open();
 };
 </script>
