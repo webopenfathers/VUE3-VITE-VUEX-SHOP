47129df6ee5fbc28c416cccfe3bc1dbf03fed7d2
webopenfathers <1290443925@qq.com>
2022-09-07 21:09:14 +0800

    2022 9-7 21:08 公告列表分页交互

--- src/pages/notice/list.vue
+++ src/pages/notice/list.vue
@@ -1,3 +1,80 @@
 <template>
-  <div>公告列表</div>
+  <el-card shadow="never" class="border-0">
+    <!-- 新增|刷新 -->
+    <div class="flex items-center justify-between mb-4">
+      <el-button type="primary" size="small">新增</el-button>
+      <el-tooltip effect="dark" content="刷新数据" placement="top">
+        <el-button text @click="getData">
+          <el-icon :size="20"><Refresh /></el-icon>
+        </el-button>
+      </el-tooltip>
+    </div>
+
+    <el-table :data="tableData" stripe style="width: 100%" v-loading="loading">
+      <el-table-column prop="title" label="公告标题" />
+      <el-table-column prop="create_time" label="发布时间" />
+      <el-table-column label="操作" width="180" align="center">
+        <template v-slot="{ row }">
+          <el-button type="primary" size="small" text>修改</el-button>
+          <el-popconfirm
+            title="是否要删除该公告?"
+            confirm-button-text="确认"
+            cancel-button-text="取消"
+            @confirm="handleDelete(row.id)"
+          >
+            <template #reference>
+              <el-button text type="primary" size="small"> 删除 </el-button>
+            </template>
+          </el-popconfirm>
+        </template>
+      </el-table-column>
+    </el-table>
+    <!-- 分页区域 -->
+    <div class="flex items-center justify-center mt-5">
+      <el-pagination
+        background
+        layout="prev,pager, next"
+        :total="total"
+        :current-page="currentPage"
+        :page-size="limit"
+        @current-change="getData"
+      />
+    </div>
+  </el-card>
 </template>
+<script setup>
+import { ref } from "@vue/reactivity";
+import { getNoticeList } from "@/api/notice";
+
+const tableData = ref([]);
+// 加载动画
+const loading = ref(false);
+
+// 分页
+const currentPage = ref(1);
+const total = ref(0);
+const limit = ref(10);
+
+// 获取数据
+function getData(p = null) {
+  if (typeof p === "number") {
+    currentPage.value = p;
+  }
+  loading.value = true;
+  getNoticeList(currentPage.value)
+    .then((res) => {
+      tableData.value = res.list;
+      total.value = res.totalCount;
+    })
+    .finally(() => {
+      loading.value = false;
+    });
+}
+
+getData();
+
+// 删除
+const handleDelete = (id) => {
+  console.log(id);
+};
+</script>
