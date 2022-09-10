51d07f4b71b9023e03ceebf2e4b242709924cfbb
webopenfathers <1290443925@qq.com>
2022-09-10 18:43:07 +0800

    2022 9-10 18:42 树形控件自定义节点渲染

--- src/pages/access/list.vue
+++ src/pages/access/list.vue
@@ -7,7 +7,35 @@
       v-loading
       node-key="id"
       :default-expanded-keys="defaultExpendedKeys"
-    />
+    >
+      <!-- data是每一行的对象 -->
+      <template #default="{ node, data }">
+        <div class="custom-tree-node">
+          <!-- 前面的菜单or权限 -->
+          <el-tag size="small" :type="data.menu ? '' : 'info'">{{
+            data.menu ? "菜单" : "权限"
+          }}</el-tag>
+          <!-- 图标 -->
+          <el-icon v-if="data.icon" :size="16" class="ml-2">
+            <component :is="data.icon"></component>
+          </el-icon>
+          <!-- 标题 -->
+          <span>{{ data.name }}</span>
+
+          <!--右边部分 -->
+          <div class="ml-auto">
+            <el-switch
+              :modelValue="status"
+              :active-value="1"
+              :inactive-value="0"
+            />
+            <el-button text type="primary" size="small">修改</el-button>
+            <el-button text type="primary" size="small">增加</el-button>
+            <el-button text type="primary" size="small">删除</el-button>
+          </div>
+        </div>
+      </template>
+    </el-tree>
   </el-card>
 </template>
 <script setup>
@@ -26,3 +54,17 @@ const { loading, tableData, getData } = useInitTable({
   },
 });
 </script>
+<style scoped>
+.custom-tree-node {
+  flex: 1;
+  display: flex;
+  align-items: center;
+  font-size: 14px;
+  padding-right: 8px;
+}
+
+::v-deep .el-tree-node__content {
+  padding-top: 20px !important;
+  padding-bottom: 20px !important;
+}
+</style>
