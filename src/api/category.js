51ae6ad7e4cae0721ed63be91df8faaa4019f548
webopenfathers <1290443925@qq.com>
2022-09-19 22:15:17 +0800

    2022 9-19 22:14 商品分类模块开发

--- src/api/category.js
+++ src/api/category.js
@@ -1,5 +1,30 @@
 import axios from "@/axios";
 
+
+// 分类列表
 export function getCategoryList() {
     return axios.get('/admin/category')
+}
+
+
+// 增加商品分类
+export function createCategory(data) {
+    return axios.post(`/admin/category`, data)
+}
+
+// 修改商品分类
+export function updateCategory(id, data) {
+    return axios.post(`/admin/category/${id}`, data)
+}
+
+
+// 修改商品分类状态
+export function updateCategoryStatus(id, status) {
+    return axios.post(`/admin/category/${id}/update_status`, { status })
+}
+
+
+// 删除商品分类
+export function deleteCategory(id) {
+    return axios.post(`/admin/category/${id}/delete`)
 }
--- src/pages/category/list.vue
+++ src/pages/category/list.vue
@@ -1,3 +1,121 @@
 <template>
-  <div>分类列表</div>
+  <el-card shadow="never">
+    <ListHeader @create="handleCreate" @refresh="getData"></ListHeader>
+    <el-tree
+      :data="tableData"
+      :props="{ label: 'name', children: 'child' }"
+      v-loading
+      node-key="id"
+    >
+      <!-- data是每一行的对象 -->
+      <template #default="{ node, data }">
+        <div class="custom-tree-node">
+          <!-- 标题 -->
+          <span>{{ data.name }}</span>
+
+          <!--右边部分 -->
+          <div class="ml-auto">
+            <el-button text type="primary" size="small">推荐商品</el-button>
+            <span @click.stop="() => {}">
+              <el-switch
+                :modelValue="data.status"
+                :active-value="1"
+                :inactive-value="0"
+                @change="handleStatusChange($event, data)"
+              />
+            </span>
+            <el-button
+              text
+              type="primary"
+              size="small"
+              @click.stop="handleEdit(data)"
+              >修改</el-button
+            >
+
+            <span @click.stop="() => {}">
+              <el-popconfirm
+                title="是否要删除该记录?"
+                confirm-button-text="确认"
+                cancel-button-text="取消"
+                @confirm="handleDelete(data.id)"
+              >
+                <template #reference>
+                  <el-button text type="primary" size="small"> 删除 </el-button>
+                </template>
+              </el-popconfirm>
+            </span>
+          </div>
+        </div>
+      </template>
+    </el-tree>
+    <!-- 弹框 -->
+    <FormDrawer ref="formDrawerRef" :title="drawerTitle" @submit="handleSubmit">
+      <el-form
+        :model="form"
+        ref="formRef"
+        :rules="rules"
+        label-width="80px"
+        :inline="false"
+      >
+        <el-form-item label="分类名称" prop="name">
+          <el-input v-model="form.name" placeholder="分类名称"></el-input>
+        </el-form-item>
+      </el-form>
+    </FormDrawer>
+  </el-card>
 </template>
+<script setup>
+import ListHeader from "@/components/ListHeader.vue";
+import FormDrawer from "@/components/FormDrawer.vue";
+import {
+  getCategoryList,
+  createCategory,
+  updateCategory,
+  updateCategoryStatus,
+  deleteCategory,
+} from "@/api/category.js";
+import { useInitTable, useInitForm } from "@/utils/useCommon.js";
+
+const { loading, tableData, getData, handleDelete, handleStatusChange } =
+  useInitTable({
+    getList: getCategoryList,
+    onGetListSuccess: (res) => {
+      tableData.value = res;
+    },
+    delete: deleteCategory,
+    updateStatus: updateCategoryStatus,
+  });
+
+const {
+  formDrawerRef,
+  formRef,
+  form,
+  rules,
+  drawerTitle,
+  handleSubmit,
+  handleCreate,
+  handleEdit,
+} = useInitForm({
+  form: {
+    name: "",
+  },
+  rules: {},
+  getData,
+  update: updateCategory,
+  create: createCategory,
+});
+</script>
+<style scoped>
+.custom-tree-node {
+  flex: 1;
+  display: flex;
+  align-items: center;
+  font-size: 14px;
+  padding-right: 8px;
+}
+
+:deep(.el-tree-node__content) {
+  padding-top: 20px;
+  padding-bottom: 20px;
+}
+</style>
