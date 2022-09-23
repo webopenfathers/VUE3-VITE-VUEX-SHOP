e256fcc8258b60c2dfc9873170a846f857e72bff
webopenfathers <1290443925@qq.com>
2022-09-23 20:59:04 +0800

    2022 9-23 20:58 回复用户评价功能

--- src/api/goods_comment.js
+++ src/api/goods_comment.js
@@ -2,16 +2,22 @@ import axios from '@/axios'
 import { queryParams } from '@/utils/util'
 
 
-// 管理员列表
+// 评价列表
 export function getGoodsCommentList(page, query = {}) {
     let r = queryParams(query)
     return axios.get(`/admin/goods_comment/${page}${r}`)
 }
 
 
-// 修改管理员状态
+// 修改评价状态
 export function updateGoodsCommentStatus(id, status) {
     return axios.post(`/admin/goods_comment/${id}/update_status`, { status })
+}
+
+
+// 回复评价
+export function reviewGoodsComment(id, data) {
+    return axios.post(`/admin/goods_comment/review/${id}`, { data })
 }
 
 
--- src/pages/comment/list.vue
+++ src/pages/comment/list.vue
@@ -11,7 +11,93 @@
       </SearchItem>
     </Search>
 
-    <el-table :data="tableData" stripe style="width: 100%" v-loading="loading">
+    <el-table
+      default-expand-all
+      :data="tableData"
+      stripe
+      style="width: 100%"
+      v-loading="loading"
+    >
+      <el-table-column type="expand">
+        <template v-slot="{ row }">
+          <div class="flex pl-18">
+            <el-avatar
+              :size="50"
+              :src="row.user.avatar"
+              fit="fill"
+              class="mr-3"
+            ></el-avatar>
+            <div class="flex-1">
+              <h6 class="flex items-center">
+                {{ row.user.nickname || row.user.username }}
+                <small class="text-gray-400 ml-2">{{ row.review_time }}</small>
+                <el-button
+                  size="small"
+                  class="ml-auto"
+                  @click="openTextarea(row)"
+                  v-if="!row.textareaEdit && !row.extra"
+                  >回复</el-button
+                >
+              </h6>
+              {{ row.review.data }}
+              <div class="py-2">
+                <el-image
+                  v-for="(item, index) in row.review.image"
+                  :key="index"
+                  :src="item"
+                  fit="cover"
+                  style="width: 100px; height: 100px"
+                  class="rounded"
+                  :lazy="true"
+                ></el-image>
+              </div>
+
+              <!-- 回复功能文本框 -->
+              <div v-if="row.textareaEdit">
+                <el-input
+                  v-model="textarea"
+                  placeholder="请输入评价内容"
+                  clearable
+                  type="textarea"
+                  :row="2"
+                ></el-input>
+                <div class="py-2">
+                  <el-button type="primary" size="small" @click="review(row)"
+                    >回复</el-button
+                  >
+                  <el-button
+                    size="small"
+                    class="ml-2"
+                    @click="row.textareaEdit = false"
+                    >取消</el-button
+                  >
+                </div>
+              </div>
+              <template v-else>
+                <!-- 客服回复 -->
+                <div
+                  class="mt-3 bg-gray-100 p-3 rounded"
+                  v-for="(item, index) in row.extra"
+                  :key="index"
+                >
+                  <h6 class="flex font-bold">
+                    客服
+                    <el-button
+                      type="info"
+                      size="small"
+                      class="ml-auto"
+                      @click="openTextarea(row, item.data)"
+                      >修改</el-button
+                    >
+                  </h6>
+                  <p>{{ item.data }}</p>
+                </div>
+              </template>
+            </div>
+          </div>
+        </template>
+      </el-table-column>
+
       <el-table-column label="ID" width="70" align="center" prop="id" />
       <el-table-column label="商品" width="250">
         <template v-slot="{ row }">
@@ -77,10 +163,12 @@ import { ref } from "vue";
 import {
   getGoodsCommentList,
   updateGoodsCommentStatus,
+  reviewGoodsComment,
 } from "@/api/goods_comment.js";
 import Search from "@/components/Search.vue";
 import SearchItem from "@/components/SearchItem.vue";
 import { useInitTable } from "@/utils/useCommon.js";
+import { toast } from "@/utils/util";
 
 const roles = ref([]);
 
@@ -104,6 +192,7 @@ const {
     // 使用map的原因给每个对象添加一个loading状态;
     tableData.value = res.list.map((o) => {
       o.statusLoading = false;
+      o.textareaEdit = false;
       return o;
     });
     total.value = res.totalCount;
@@ -111,4 +200,23 @@ const {
   },
   updateStatus: updateGoodsCommentStatus,
 });
+
+const textarea = ref("");
+
+const openTextarea = (row, data = "") => {
+  textarea.value = data;
+  row.textareaEdit = true;
+};
+
+// 回复
+const review = (row) => {
+  if (textarea.value == "") {
+    return toast("回复内容不能为空", "error");
+  }
+  reviewGoodsComment(row.id, textarea.value).then((res) => {
+    row.textareaEdit = false;
+    toast("回复成功");
+    getData();
+  });
+};
 </script>
