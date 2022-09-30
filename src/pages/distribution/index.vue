9ad147630dce3e279128c2801ac16faa0f3d714b
webopenfathers <1290443925@qq.com>
2022-09-30 22:03:25 +0800

    2022 9-30 22:03 分销员管理开发(一)

---
+++ src/pages/distribution/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <panel />
+    分销员管理
+  </div>
+</template>
+<script setup>
+import panel from "./panel.vue";
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -19,6 +19,7 @@ import SkusList from '@/pages/skus/list.vue'
 import LevelList from '@/pages/level/list.vue'
 import SettingBuy from '@/pages/setting/buy.vue'
 import SettingShip from '@/pages/setting/ship.vue'
+import DistributionIndex from '@/pages/distribution/index.vue'
 
 
 
@@ -180,6 +181,14 @@ const asyncRoutes = [
         component: SettingShip,
         meta: {
             title: '物流设置'
+        }
+    },
+    {
+        path: '/distribution/index',
+        name: '/distribution/index',
+        component: DistributionIndex,
+        meta: {
+            title: '分销员管理'
         }
     },
 ]
