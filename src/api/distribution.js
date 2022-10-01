48cf9cb98cdc1e09cfb0f43955e45a83e2cb1f04
webopenfathers <1290443925@qq.com>
2022-10-01 17:25:36 +0800

    2022 10-1 17:25 分销员管理开发(二)

--- src/api/distribution.js
+++ src/api/distribution.js
@@ -1,4 +1,12 @@
 import axios from "@/axios";
+import { queryParams } from '@/utils/util'
+
+
+// 订单列表
+export function getAgentList(page, query = {}) {
+    let r = queryParams(query)
+    return axios.get(`/admin/agent/${page}${r}`)
+}
 
 export function getAgentStatistics() {
     return axios.get('/admin/agent/statistics')
